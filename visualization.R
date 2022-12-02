7eada576a2ee27b21ca74243609cfe77133ab04b
Kajalben Bharatbhai Patel <kp22aba@herts.ac.uk>
2022-12-02 21:35:33 +0000

    Add colour to the visualization and clean the script

--- visualization.R
+++ visualization.R
@@ -4,7 +4,9 @@ library(tidyverse)
 df <- read_csv("ghana-food-prices-1.csv")
 
 # Filter the Dataframe to get the 
-dfRiceOnly <- df %>% filter(cmname %in% c("Rice (local) - Wholesale", "Rice (imported) - Wholesale"))
+dfRiceOnly <- df %>%
+              filter(cmname %in% c("Rice (local) - Wholesale",
+                                    "Rice (imported) - Wholesale"))
 
 pdf("visualization.pdf")
 
@@ -18,7 +20,10 @@ y <- as.numeric(dfRiceOnly$price)
 boxplot(y ~ x,
         main = "Boxplot of Commodity Name against Price",
         xlab = "Commodity Name",
-        ylab = "Price"
+        ylab = "Price",
+        boxlty = 5,
+        boxfill = "beige",
+        boxcol = "cadetblue1"
         )
 
 # Histogram
@@ -30,7 +35,9 @@ yMean <- mean(y)
 ySd <- sd(y)
 
 h <- hist(
-  y, breaks = 5, density = 10 # Change the value of 'breaks' to get a better histogram
+  y, breaks = 8, density = 10,
+  border = "blue",
+  col = "aquamarine"
 )
 
 w <- seq(yMin, yMax, 1)
@@ -38,7 +45,7 @@ z <- dnorm(w, mean = yMean, sd = ySd)
 
 z1 <- z * diff(h$mid[1:2]) * length(y)
 
-lines(w, z1, col = "green")
+lines(w, z1, col = "bisque")
 
 dev.off()
 
